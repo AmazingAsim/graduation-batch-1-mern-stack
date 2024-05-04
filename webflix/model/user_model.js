9a8833424cb80e992d9df13b685e334de89d8f22
AmazingAsim <101821929+AmazingAsim@users.noreply.github.com>
2024-05-04 19:52:12 +0530

    class 12th

--- webflix/model/user_model.js
+++ webflix/model/user_model.js
@@ -4,10 +4,9 @@ let userSchema = mongoose.Schema({
     user_name:{type:String,required:true},
     user_email:{type:String,required:true},
     user_password:{type:String,required:true},
-    user_profile:{type:String,required:false},
-    user_role:{type:String,required:true}
+    user_profile:{type:String,required:false}
 })
 
-let userModel = require('users',userSchema);
+let userModel = mongoose.model('users',userSchema);
 
 module.exports = userModel;
--- webflix/router/user_routes.js
+++ webflix/router/user_routes.js
@@ -1,7 +1,17 @@
 let express  = require('express');
-
 let router = express.Router();
+let userController = require('../controller/userController')
 
+router.get('/',userController.getusers);
 
+router.post('/signup',userController.signUp);
+
+router.post('/login',userController.login);
+
+// router.get('/getUserById/:id',auth.checkJwtToken,userController.currentUser)
+
+// router.post('/addProfile',upload.single('profileImage',userController.addProfile));
+
+// router.get('/getimage/:profileImage',userController.getImage)
 
 module.exports = router;
